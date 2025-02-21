6741c83b6746d8a7682b950ae9d46ac14acc80c9
Five-great <43601963+Five-great@users.noreply.github.com>
2025-02-21 09:34:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,67 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
+
+
+const axios = require('axios');
+const querystring = require('querystring');
+
+
+let getProxyInfoData = (req, res, proxyUrl)=>{
+
+    var headersData = {
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9",
+        Connection: "keep-alive",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        Host: "h5.hunlihu.com",
+        Origin: "https://s.hunlihu.com",
+        Referer: "https://s.hunlihu.com/",
+        "sec-ch-ua": "\".Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"103\", \"Chromium\";v=\"103\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "Windows",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-site",
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.66 Safari/537.36",
+    };
+    let body = req.body
+//   let formData = {
+//     'auth': 'Lz8rzL8zzt',
+//     'jsons': `[{"d_name":"five123","user_agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.66 Safari/537.36","msg_type":"1","openid":"","nickname":"哈哈1","headimgurl":"","d_else":"新婚快乐 百年好合"}]`
+
+//   }
+return new Promise((resolve,reject)=>{
+   let formData = body
+
+// 目标服务器地址和端口
+const options = {
+
+headers: headersData
+};
+
+// let url= "https://h5.hunlihu.com/vashow/ly/door/door/sign2";
+let url= "https://h5.hunlihu.com"+proxyUrl;
+
+
+// 将对象格式的数据转换为 x-www-form-urlencoded 格式
+        const encodedData = querystring.stringify(formData);
+
+        // 更新请求头中的 Content-Length
+        options.headers['Content-Length'] = Buffer.byteLength(encodedData);
+        axios.post(url, encodedData, { headers:options.headers })
+        .then(response => {
+            console.log('请求成功，响应数据:', req,response.data);
+            resolve(response.data)
+        }).catch((err)=>{
+            reject(err)
+        })
+ })
+}
+    
+
+
+
+
 // 创建代理服务器实例
 const proxy = httpProxy.createProxyServer({
    // target: 'https://s.hunlihu.com', // 替换为你要代理的目标服务器地址
@@ -13,7 +75,8 @@ const subdirectoryMappings = {
      'h5hunlihu': 'https://h5.hunlihu.com',
 };
 // 创建HTTP服务器
-const server = http.createServer((req, res) => {
+const server = http.createServer(async(req, res) => {
+    
 
    if(new RegExp(`^\/shunlihu\/`).test(req.url)){
         // proxyRes.headers =  
@@ -43,14 +106,22 @@ const server = http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Headers', req.headers['access-control-request-headers'] || '*');
     // 允许携带凭证（如 cookies）
     res.setHeader('Access-Control-Allow-Credentials', 'true');
-      proxy.options.target=subdirectoryMappings.h5hunlihu;
-       
-         req.headers['origin'] = subdirectoryMappings.shunlihu;
-         req.headers['referer'] = subdirectoryMappings.shunlihu;
-         req.headers['host'] = "h5.hunlihu.com";
-         req.url = req.url.replace(new RegExp(`^\/shunlihu`), '');
-    // 将请求代理到目标服务器
-     proxy.web(req, res);
+    https://h5.hunlihu.com/vashow/ly/door/door/init?0.9173087912286308
+    if(/\/vashow\/ly\/door\/door\/init/.test(req.url)){
+       let proxyData = await getProxyInfoData(req, res,req.url.replace(/^\/shunlihu/, ''))
+       res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
+        res.end(proxyData);
+    }else{
+        proxy.options.target=subdirectoryMappings.h5hunlihu;
+        req.headers['origin'] = subdirectoryMappings.shunlihu;
+        req.headers['referer'] = subdirectoryMappings.shunlihu;
+        req.headers['host'] = "h5.hunlihu.com";
+        req.url = req.url.replace(new RegExp(`^\/shunlihu`), '');
+      // 将请求代理到目标服务器
+        proxy.web(req, res);
+    }
+      
+      
    }
 });
 // 监听端口
